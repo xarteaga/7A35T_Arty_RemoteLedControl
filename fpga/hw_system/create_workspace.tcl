4b4613f7d1e6ce202eb7c3ac051ee7bb2c0d7372
Vaxi Atrea Ramnit <xavie.arteaga@gmail.com>
2016-09-15 21:05:48 +0200

    Add automated bootloader bitfile generation

--- fpga/hw_system/create_workspace.tcl
+++ fpga/hw_system/create_workspace.tcl
@@ -18,8 +18,13 @@ sdk create_bsp_project -name srec_spi_bootloader_bsp -hwproject hw_0 -mss src/bo
 sdk create_app_project -name helloworld -hwproject hw_0 -proc microblaze_0 -os standalone -lang C -app {Hello World} -bsp bsp_0
 
 # 6) Create Bootloader application
-sdk create_app_project -name srec_spi_bootloader -hwproject hw_0 -proc microblaze_0 -os standalone -lang C -app {SREC Bootloader} -bsp srec_spi_bootloader_bsp
+sdk create_app_project -name srec_spi_bootloader -hwproject hw_0 -proc microblaze_0 -os standalone -lang C -app {SREC SPI Bootloader} -bsp srec_spi_bootloader_bsp
 
+# 7) Modify bootloader pointer
+exec echo "#undef FLASH_IMAGE_BASEADDR" >> ../sw_system/sdk_workspace/srec_spi_bootloader/src/blconfig.h
+exec echo "#define FLASH_IMAGE_BASEADDR 0x400000" >> ../sw_system/sdk_workspace/srec_spi_bootloader/src/blconfig.h
+
+# 8) Compile all projects
 sdk build_project bsp_0
 sdk build_project srec_spi_bootloader_bsp
 sdk build_project helloworld
